5f388ff01b5716a7d3adbce41f63ed490bfadfce
Ranjith <ranju08134@gmail.com>
2019-10-17 18:02:33 +0530

    UPdated

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 var score,roundScore,activePlayer;
 scores=[0,0];
 roundScore=0;
-activePlayer=1;
+activePlayer=0;
 // // Changing Palyer score
 // document.querySelector('#current-'+ activePlayer).textContent=dice;
 // var x=document.querySelector("#score-0").textContent;
 // console.log(x); 
 // Hiding dice
 document.querySelector('.dice').style.display='none';
-// document.getElementById('score-0').textContent=0;
-// document.getElementById('score-1').textContent=0;
-// document.getElementById('current-0').textContent=0;
-// document.getElementById('current-0').textContent=0;
+document.getElementById('score-0').textContent=0;
+document.getElementById('score-1').textContent=0;
+document.getElementById('current-0').textContent=0;
+document.getElementById('current-1').textContent=0;
 // Adding event listener for rolling botton
 document.querySelector('.btn-roll').addEventListener('click',function(){
     // Random number
@@ -22,5 +22,21 @@ document.querySelector('.btn-roll').addEventListener('click',function(){
     diceDOM.style.display='block';
     diceDOM.src='./images/dice-' + dice + '.png';
     // Update the round score if the rolled number is greater than 1 otherwise we loose current score and next palyer turn
-
+     if(dice !== 1){
+        //  Add score
+         roundScore+=dice;
+         document.querySelector('#current-'+ activePlayer).textContent=roundScore;
+     }
+     else{
+        //  Next player
+        activePlayer===0 ? activePlayer=1:activePlayer=0;
+        roundScore=0;
+        document.getElementById('current-0').textContent=0;
+        document.getElementById('current-1').textContent=0;
+        document.querySelector('.player-0-panel').classList.toggle('active');
+        document.querySelector('.player-1-panel').classList.toggle('active');
+        document.querySelector('.dice').style.display='none';
+        // document.querySelector('.player-0-panel').classList.remove('active');
+        // document.querySelector('.player-1-panel').classList.add('active');
+     }
 })
